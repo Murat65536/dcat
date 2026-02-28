b5e8dab78ed6ff847c45257e6337bc6b93ba4d74
murat <99989538+Murat65536@users.noreply.github.com>
2026-02-28 00:32:27 -0500

    Fix sixel newline

--- src/terminal/sixel.c
+++ src/terminal/sixel.c
@@ -14,8 +14,21 @@ static sixel_output_t *sixel_out = NULL;
 static sixel_dither_t *sixel_dith = NULL;
 static uint8_t *sixel_pixels = NULL;
 static size_t sixel_pixels_cap = 0;
+static bool sixel_initialized = false;
+
+static void sixel_cleanup(void) {
+    /* Re-enable sixel scrolling */
+    safe_write("\x1b[?80l", 6);
+}
 
 void render_sixel(const uint8_t *buffer, uint32_t width, uint32_t height) {
+    if (!sixel_initialized) {
+        /* Disable sixel scrolling */
+        safe_write("\x1b[?80h", 6);
+        atexit(sixel_cleanup);
+        sixel_initialized = true;
+    }
+
     safe_write("\x1b[H", 3);
 
     size_t data_size = (size_t)width * height * 4;
